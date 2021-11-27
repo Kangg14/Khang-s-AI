e04f2355c4b9e78a37a6db228fca0e1385886d5d
MinhKangCoder <93512392+MinhKangCoder@users.noreply.github.com>
2021-11-27 16:49:21 +0700

    Add files via upload

---
+++ hieu.py
@@ -0,0 +1,12 @@
+you = "hello"
+
+if you == "":
+    robot_brain = "I can't hear you, can you try again"
+elif you == "hello":
+    robot_brain = "Hello Khang"
+elif you == "Today":
+    robot_brain = "Thu 6"
+else:
+    robot_brain = "I'm fine thank you and you"
+
+    print(robot_brain)
